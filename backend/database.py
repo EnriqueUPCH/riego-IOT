8ab3ff9251ab5becd34b7cac4bc555bd77c72db9
Your Name <enrique.orozco@upch.pe>
2026-06-24 17:02:23 -0500

    Busca cambios

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql://admin:admin123@136.248.71.101:5432/riego"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:admin123@136.248.71.101:5432/riego"
+)
 
 engine = create_engine(DATABASE_URL)
 
